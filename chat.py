f79a7de97ab3a598a2e3adf58f156d1d1fff3080
JR <allen8297@gmail.com>
2020-07-15 22:56:57 +0800

    basic version done

---
+++ chat.py
@@ -0,0 +1,34 @@
+def read_file(file_name):
+	lines = []
+	with open(file_name, 'r', encoding = 'utf-8-sig') as f: # -sig為刪除存檔編碼資料之編碼方式
+		for line in f:
+			lines.append(line.strip())
+	return(lines)
+
+def convert(lines):
+	new = []
+	person = None # 虛無, 用於不存在的部分
+	for line in lines:
+		if line == 'Allen':
+			person = 'Allen'
+			continue
+		elif line == 'Tom':
+			person = 'Tom'
+			continue
+		if person:
+			new.append(person + ': ' + line)
+	return(new)
+
+def write_file(file_name, lines):
+	with open(file_name, 'w', encoding='utf-8-sig') as f:
+		for line in lines:
+			f.write(line + '\n')
+
+def main():
+	lines = read_file('input.txt')
+	lines = convert(lines)
+	write_file('output.txt', lines)
+	# print(lines)
+
+
+main()
